f5f34d340bd218d8be09f3d1cb23f778b315a872
Shail Patel <shailp2310@gmail.com>
2024-01-18 17:00:52 +0530

    Initial Commit

---
+++ DemoTaskAjmeraInfotech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoTaskAjmeraInfotech
+//
+//  Created by Shail Patel on 18/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
